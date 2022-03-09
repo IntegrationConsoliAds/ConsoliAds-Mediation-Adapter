1a51d42086ff3ad42a13847d6e16506276c4d8c1
Wazma Ali <wazma.ali@consoliads.com>
2022-03-09 18:14:28 +0500

    version updated v_13.0.0-1.0

--- ConsoliAds-Mediation-Adapter/ConsoliAdsMediationAdapter.framework/Headers/ConsoliAdsMediationAdapter.h
+++ ConsoliAds-Mediation-Adapter/ConsoliAdsMediationAdapter.framework/Headers/ConsoliAdsMediationAdapter.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double ConsoliAdsMediationAdapterVersionNumber;
 
 //! Project version string for ConsoliAdsMediationAdapter.
 FOUNDATION_EXPORT const unsigned char ConsoliAdsMediationAdapterVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ConsoliAdsMediationAdapter/PublicHeader.h>
-
-
